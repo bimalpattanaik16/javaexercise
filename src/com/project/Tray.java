1733638b97098dbe1de9948dd585af87d2195844
bimalpattanaik16 <bimalpattanaik16@gmail.com>
2021-11-19 09:42:41 +0530

    project

---
+++ src/com/project/Tray.java
@@ -0,0 +1,56 @@
+package com.project;
+
+import java.util.List;
+
+import com.library.Book;
+import com.library.Student;
+
+public class Tray {
+	private List<Book> book;
+	private List<Student> students;
+	private int cupboardList;
+	private int occupiedList;
+	private int unoccupiedList;
+	public Tray(List<Book> book, List<Student> students) {
+		this.book = book;
+		this.students = students;
+		cupboardList=100;
+		occupiedList=0;
+		unoccupiedList=0;
+	}
+	public List<Book> getBook() {
+		return book;
+	}
+	public void updateBook(Book book) {
+		this.book.add(book);
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void updateStudents(Student students) {
+		this.students.add(students);
+	}
+	public int getCupboardList() {
+		return cupboardList;
+	}
+	public void updateCupboardList(int cupboardList) {
+		cupboardList+= cupboardList;
+	}
+	public int getOccupiedList() {
+		return occupiedList;
+	}
+	public void updateOccupiedList(int occupiedList) {
+		occupiedList += occupiedList;
+	}
+	public int getUnoccupiedList() {
+		return unoccupiedList;
+	}
+	public void updateUnoccupiedList(int unoccupiedList) {
+		unoccupiedList += unoccupiedList;
+	}
+	
+	
+	
+}
+
+
